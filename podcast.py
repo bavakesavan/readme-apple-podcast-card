e44ac3a57d6e3a72f1f58d9c521749fa73a86d6e
Bava Kesavan <bkesavan@evertz.com>
2023-06-17 20:11:10 -0400

    Adding app.py

---
+++ podcast.py
@@ -0,0 +1,5 @@
+class Podcast:
+    def __init__(self, artist_name, track_name, artwork_url):
+        self.artist_name = artist_name
+        self.track_name = track_name
+        self.artwork_url = artwork_url
