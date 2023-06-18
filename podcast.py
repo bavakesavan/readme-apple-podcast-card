39bf41dd2550086d16394e7fd79cc73179463981
Bava Kesavan <123393825+bavak@users.noreply.github.com>
2023-06-17 20:14:54 -0400

    Revert "Adding app.py"

--- podcast.py
+++
@@ -1,5 +0,0 @@
-class Podcast:
-    def __init__(self, artist_name, track_name, artwork_url):
-        self.artist_name = artist_name
-        self.track_name = track_name
-        self.artwork_url = artwork_url
